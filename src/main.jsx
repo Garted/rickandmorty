23f36a47e3103fbaf68a24188715d2edb6ab6693
Eugeny Kurnysh <garted93@gmail.com>
2025-02-21 13:24:57 +0300

    fix: add basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import store from './store/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/rickandmorty">
       <Provider store={store}>
         <App />
       </Provider>
